fe9149cbf351f8cc1399d3513199d5979ce8cd57
Pierre Queinnec <pierre@queinnec.org>
2014-04-19 17:02:47 +0200

    Add a crude debug log for the AES IV.

--- src/com/amazonaws/tvm/AESEncryption.java
+++ src/com/amazonaws/tvm/AESEncryption.java
@@ -59,8 +59,20 @@ public class AESEncryption {
 		
 		// DEBUG
 		Utilities.log.info("IV = " + Arrays.toString(iv));
-		Utilities.log.info("IV as String = " + new String(iv));
+		Utilities.log.info("IV as hex = " + bytesToHex(iv));
 
 		return iv;
 	}
+
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+
 }
